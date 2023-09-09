8248a61304a2e9dde84f86b5b48491235ffb0c36
varga madalina <madalina.g.varga@gmail.com>
2023-09-09 11:06:56 +0300

    added react quey with an example

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,8 +1,20 @@
+import { useQuery } from "react-query";
+
 function Home() {
+  const { isLoading, error, data } = useQuery("todos", () =>
+    fetch("https://jsonplaceholder.typicode.com/todos").then((res) => res.json())
+  );
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>An error has occurred: {(error as any).message}</p>;
+
   return (
-    <div>
-      <p className="bg-red-500">Home</p>
-    </div>
+    <ul>
+      {data.map((todo: any) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
   );
 }
 
